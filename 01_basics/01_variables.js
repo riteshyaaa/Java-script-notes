ba16997965b22d9285f949f13f51c5d4a2814b0d
RITESH <riteshyaaa@gmail.com>
2024-07-27 06:14:51 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=14422
+let accountEmail="riteshyaa@gmail.com"
+var accountPassword="1234"
+accountCity= "jaipur"
+console.table([accountId,accountEmail,accountCity]);
+accountCity="bangaluru"
+accountEmail="prateek@gmail.com"
