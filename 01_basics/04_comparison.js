d510ed2ce0aff6e2d0010427ae1d53563d97401f
RITESH <riteshyaaa@gmail.com>
2024-07-27 18:35:39 +0000

    comaprison in javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2==1)
+// console.log(2>2);
+// console.log(2<2);
+// console.log(3>2);
+// console.log(-2==-2);
+
+
+// console.log( "2"==2);// true 
+
+// console.log(1<"2");//true 
+// console.log(1>"2"); // false
+
+console.log(null>0); // False 
+console.log(null==0); // False equality comaprison type not convert 
+console.log(null>=0); // True  comparison greater datatype convert null into number
