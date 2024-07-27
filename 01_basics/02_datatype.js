ecf82c7fcb50c384d95a6d49e5a92b6d22224799
RITESH <riteshyaaa@gmail.com>
2024-07-27 16:04:41 +0000

    container config done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+let state;
+console.log(state) //undefined 
+console.log("state") //print state
+
+
+
+//alert(3+3) //we are using nodejs,not brower
+console.log(3+3);
+console.log("ritesh") //code readability should be high
+
+
+// number =>2 to power 53
+// bigint
+
+// boolean => true /false 
+// null => standalone value 
+// undefined => 
+   //  symbol=> unique
+// object
+console.log(typeof "age" )
