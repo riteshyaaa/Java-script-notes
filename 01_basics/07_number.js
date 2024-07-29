6fc196e9e370923923e6b7767c7f38d0c4be4ca8
RITESH <riteshyaaa@gmail.com>
2024-07-29 18:18:46 +0000

    number done

---
+++ 01_basics/07_number.js
@@ -0,0 +1,27 @@
+const balance = 100
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+const othernumber = 125.197
+//console.log(othernumber.toPrecision(1));
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+//it is very useful 
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1+1);
+const min = 10
+const max= 20
+
+
+console.log(Math.floor(Math.random()*(max - min +1)) + min)
