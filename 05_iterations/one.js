dfaa744b2f2389cba4c0c7f0de5eb9ca7e762479
RITESH <riteshyaaa@gmail.com>
2024-08-04 17:55:28 +0000

    done

---
+++ 05_iterations/one.js
@@ -0,0 +1,18 @@
+//for 
+
+// for (let index = 0; index <10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+
+for (let i = 0; i <=10; i++) {
+    console.log(`Outer loop value : ${i}`);
+    
+     for (let j = 0; j <= 10; j++) {
+    //console.log(`Innner loop value${j} and outer loop ${i}`);
+     console.log(i +'*' + j + '='+ i*j )
+         
+     }
+     
+     
+ }
