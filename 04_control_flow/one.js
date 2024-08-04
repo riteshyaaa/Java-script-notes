e50d5ed5c3e9d8102abb380d7a460d555103b35a
RITESH <riteshyaaa@gmail.com>
2024-08-04 17:51:55 +0000

    controle flow done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,42 @@
+const month = "January";
+switch(month){
+
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    // case 3:
+    //     console.log("March");
+    //     break;
+    // case 4:
+    //     console.log("April");
+    //     break;
+    // case 5:
+    //     console.log("May");
+    //     break;
+    // case 6:
+    //     console.log("June");
+    //     break;
+    // case 7:
+    //     console.log("July");
+    //     break;
+    // case 8:
+    //     console.log("August");
+    //     break;
+    // case 9:
+    //     console.log("September");
+    //     break;
+    // case 10:
+    //     console.log("October");
+    //     break;
+    // case 11:
+    //     console.log("November");
+    //     break;
+    // case 12:
+    //     console.log("December");
+    //     break;
+    default:
+        console.log("Invalid month number");
+}
